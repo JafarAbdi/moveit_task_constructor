5760b5d98d2d2eee80ef291b4d4b4b3630621aae
JafarAbdi <cafer.abdi@gmail.com>
2021-09-21 09:49:59 +0300

    Galactic compatibility

--- core/src/solvers/pipeline_planner.cpp
+++ core/src/solvers/pipeline_planner.cpp
@@ -42,7 +42,12 @@
 #include <moveit/planning_pipeline/planning_pipeline.h>
 #include <moveit_msgs/msg/motion_plan_request.hpp>
 #include <moveit/kinematic_constraints/utils.h>
+
+#if __has_include(<tf2_eigen/tf2_eigen.hpp>)
+#include <tf2_eigen/tf2_eigen.hpp>
+#else
 #include <tf2_eigen/tf2_eigen.h>
+#endif
 
 namespace moveit {
 namespace task_constructor {
--- core/src/stages/move_to.cpp
+++ core/src/stages/move_to.cpp
@@ -43,6 +43,12 @@
 #include <moveit/task_constructor/stages/move_to.h>
 #include <moveit/task_constructor/cost_terms.h>
 #include <rviz_marker_tools/marker_creation.h>
+#if __has_include(<tf2_eigen/tf2_eigen.hpp>)
+#include <tf2_eigen/tf2_eigen.hpp>
+#else
+#include <tf2_eigen/tf2_eigen.h>
+#endif
+#include <moveit/robot_state/conversions.h>
 
 namespace moveit {
 namespace task_constructor {
